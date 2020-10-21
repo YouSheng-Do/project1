f1eea66c20391478a080660a84ad4691e9a616bb
YouSheng-Do <58830729+YouSheng-Do@users.noreply.github.com>
2020-10-22 02:56:16 +0800

    Add files via upload

--- 107062209_proj1_first.cpp
+++ 107062209_proj1_first.cpp
@@ -8,6 +8,11 @@
 using namespace std;
 
 void generate_new_block(string block, int pos,int dis);
+void move_(int temp[4][4],int pos,int dis);
+void eliminate();
+int row,column;
+int **board;
+
 int T1[4][4] = {{0,0,0,0},
                 {0,0,0,0},
                 {1,1,1,0},
@@ -116,7 +121,7 @@ int I2[4][4] = {{0,0,0,0},
                 {1,1,1,1}
                 };
 
-int O[4][4] = {{0,0,0,0},
+int O[4][4] = { {0,0,0,0},
                 {0,0,0,0},
                 {1,1,0,0},
                 {1,1,0,0}
@@ -125,29 +130,15 @@ int O[4][4] = {{0,0,0,0},
 
 int main(int argc,char* argv[]){
     ifstream file;
+    ofstream file_out;
     string s;
+    file_out.open("107062209_proj1.final");
     file.open(argv[1]);
-    int row,column;
 
     file>>row;
     file>>column;
 
-    while(1){
-        string block;
-        int pos,dis;
-        file>>block;
-        if(block=="End"){
-            block = "";
-            break;
-        }
-        file>>pos;
-        file>>dis;
-        generate_new_block(block,pos,dis);
-        //cout<<block<<" "<<pos<<" "<<dis<<endl;
-    }
-
-
-    int **board = new int*[row+1];
+    board = new int*[row+1];
 
     for(int i = 0;i < row;i++){
         board[i]= new int[column+2];
@@ -158,11 +149,32 @@ int main(int argc,char* argv[]){
     for(int j = 0;j<column+2;j++)
         board[row][j]=1;
 
+    while(1){
+        string block;
+        int pos,dis;
+        file>>block;
+        if(block=="End"){
+            block = "";
+            break;
+        }
+        file>>pos;
+        pos;
+        file>>dis;
+        generate_new_block(block,pos,dis);
+        eliminate();
+        //cout<<block<<" "<<pos<<" "<<dis<<endl;
+    }
 
-    for(int i=0;i<row+1;i++){
+    /*for(int i=0;i<row+1;i++){
         for(int j=0;j<column+2;j++)
             cout<<board[i][j];
         cout<<endl;
+    }*/
+
+    for(int i = 0;i < row;i++){
+        for(int j = 1;j<column+1;j++)
+            file_out<<board[i][j]<<" ";
+        file_out<<endl;
     }
 
     for(int i = 0;i < row;i++){
@@ -173,6 +185,7 @@ int main(int argc,char* argv[]){
     board = NULL;
 
     file.close();
+    file_out.close();
     return 0;
 }
 
@@ -185,7 +198,13 @@ void generate_new_block(string block, int pos,int dis){
                 cout<<temp[i][j];
             }
             cout<<endl;
-        }*/
+        }
+        for(int i =0;i<row+1;i++){
+        for(int j = 0;j<column+2;j++){
+            cout<<board[i][j];
+        }
+        cout<<endl;
+    }*/
     }
     else if(block=="T2"){
         memcpy(temp,T2,sizeof(T2));
@@ -245,4 +264,134 @@ void generate_new_block(string block, int pos,int dis){
         memcpy(temp,O,sizeof(O));
 
     }
+    move_(temp,pos,dis);
 }
+void move_(int temp[4][4],int pos,int dis){
+    int finish=0;
+    int x = pos;
+    int touch_bottom = 0;
+    int touch_boundary = 0;
+    int y = 0;
+    /*for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<temp[i][j];
+        }
+        cout<<endl;
+    }
+    for(int i =0;i<row+1;i++){
+        for(int j = 0;j<column+2;j++){
+            cout<<board[i][j];
+        }
+        cout<<endl;*/
+    //}
+
+    while(!finish){
+        while(!touch_bottom){
+            /*cout<<"x "<<x<<"y "<<y<<endl;
+            cout<<board[y+1][x]<<temp[3][0]<<endl;
+            cout<<board[y+1][x+1]<<temp[3][1]<<endl;
+            cout<<board[y+1][x+2]<<temp[3][2]<<endl;
+            cout<<board[y+1][x+3]<<temp[3][3]<<endl;*/
+            if(((board[y+1][x]==1)&&(temp[3][0]==1))||((board[y+1][x+1]==1)&&(temp[3][1]==1))||
+               ((board[y+1][x+2]==1)&&(temp[3][2]==1))||((board[y+1][x+3]==1)&&(temp[3][3]==1))){
+                touch_bottom = 1;
+            }
+            else{
+                y++;
+             //   cout<<"y = "<<y<<endl;
+             //   cout<<"x = "<<x<<endl;
+            }
+            /*for(int i =0;i<row+1;i++){
+                for(int j = 0;j<column+2;j++){
+                    cout<<board[i][j];
+                }
+                cout<<endl;
+            }*/
+        }
+        if(touch_bottom){
+            //cout<<"x "<<x<<"y "<<y<<endl;
+            if(dis>0){
+                if((board[y][x+1]==1&&temp[y][3]==1)||(board[y-1][x+1]==1&&temp[y-1][3]==1)
+                   ||(board[y-2][x+1]==1&&temp[y-2][3]==1)||(board[y-3][x+1]==1&&temp[y-3][3]==1))
+                    touch_boundary = 1;
+                else{
+                    dis--;
+                    x++;
+                    touch_bottom = 0;
+                }
+
+                //cout<<"dis:"<<dis<<endl;
+            }
+            else if(dis<0){
+                if((board[y][x-1]==1&&temp[y][0]==1)||(board[y-1][x-1]==1&&temp[y-1][0]==1)
+                   ||(board[y-2][x-1]==1&&temp[y-2][0]==1)||(board[y-3][x-1]==1&&temp[y-3][0]==1))
+                    touch_boundary = 1;
+                else{
+                    dis++;
+                    x--;
+                    touch_bottom = 0;
+                }
+                //cout<<"dis:"<<dis<<endl;
+            }
+        }
+        if(touch_bottom){
+            finish = 1;
+            //cout<<"finish"<<endl;
+            //cout<<"x"<<x<<"y"<<y<<endl;
+            for(int i = y-3;i<=y;i++){
+                for(int j = x;j<x+4&&j<=column;j++){
+                    board[i][j] = temp[i+3-y][j-x]||board[i][j];
+                    //cout<<board[i][j];
+                }
+                //cout<<endl;
+            }
+        }
+    }
+}
+void eliminate(){
+    int *all_1 = new int[column+2];
+    int index = 0;
+    int same = 1;
+    int flag = 1;
+    for(int i=0;i<column+2;i++){
+        all_1[i] = 1;
+    }
+    while(flag){
+        flag=0;
+        for(int i=0;i<row;i++){
+            same = 1;
+            for(int j = 0;j<column+2;j++){
+                if(board[i][j]!=all_1[j]){
+                    same = 0;
+                }
+            }
+            if(same){
+                index = i;
+                flag = 1;
+                break;
+            }
+        }
+        //cout<<"index"<<index<<endl;
+        //cout<<"flag"<<flag<<endl;
+        if(flag){
+            for(int i = index;i>0;i--){
+                for(int j=0;j<column+2;j++){
+                    board[i][j]=board[i-1][j];
+                }
+            }
+            for(int i = 1;i<column+1;i++){
+                board[0][i] = 0;
+            }
+        }
+        /*for(int i=0;i<row+1;i++){
+        for(int j=0;j<column+2;j++)
+            cout<<board[i][j];
+        cout<<endl;
+    }
+    cout<<endl;*/
+    }
+    free(all_1);
+}
+/*int collision(int **temp,int x,int y){
+
+}*/
